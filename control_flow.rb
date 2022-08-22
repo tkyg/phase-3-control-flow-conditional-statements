6239d7736e64a98f01a7b0abf222b6c567fdca01
tkyg <80114433+tkyg@users.noreply.github.com>
2022-08-22 11:55:30 -0400

    done

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,56 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+    if (username == "admin" || username == "ADMIN") && password == "12345"
+      "Access granted"
+    else 
+      "Access denied"
+  end
 end
+
 
 def hows_the_weather(temperature)
-  # your code here
+  case temperature
+  when 40..65
+    "It's a little chilly out there!"
+  when 65..85
+    "It's perfect out there!"
+  else 
+    if temperature <=40
+      "It's brisk out there!"
+    elsif temperature >= 85
+      "It's too dang hot out there!"
+    end
+  end
 end
+  
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else
+    num
+  end
 end
+
 
 def calculator(operation, num1, num2)
-  # your code here
+  # your code hereadmin
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else 
+    puts "Invalid operation!"
+    return nil
 end
-
+end
